5924c5d49d045b25b6782a19dc7a39c9ea5f2334
nyghtowl <warrick.melanie@gmail.com>
2013-06-24 22:39:42 -0700

    Structure refactored to object oriented.

---
+++ markov_model2.rb
@@ -0,0 +1,123 @@
+
+'''
+Make Markov chain 
+
+create two hashes to hold markov chains
+upper case keys hash
+lower case keys hash 
+both can hold values with upper and lower
+values stripped of all characters except end characters
+keys stripped of all characters
+
+'''
+# message = []
+# head = Cap.keys.sample
+# end = false
+# while not end(message)
+#   pick source
+#   choose from source
+#   if no choice
+#     choice = choose random new head
+#   message << choice
+#   head = choice
+
+ 
+# a head is a start of a sentence, and a tail is any word that follows
+# that head in the given sample text.
+class Markov
+    END_MARKS = [".", "?", "!"]
+    CAPS = "A".."Z"
+
+    def initialize(str)
+        '''
+        Takes in a string and returns dict of markov chains.
+        '''
+        #create hash with word pairings
+        @lower = Hash.new { |hash, key| hash[key] = [] } #lower case keys
+        @cap = Hash.new { |hash, key| hash[key] = [] } #upper case keys
+
+        #clean string of characters and change to list
+        word_array = remove_punctuation(str).split
+        #loop through string and generate word dict
+        word_array.each_with_index do |head, num|
+            next if num > word_array.length - 1
+
+            head = collapse_heads(head)
+            tail = word_array[num+1]
+
+            #assign key value pair to either lower or cap hash
+            hash = if is_cap(head)
+                @cap
+            else                
+                @lower
+            end
+
+            hash[head] << tail
+        end
+    end
+
+    def generate()
+        message = []
+        head = @cap.keys.sample
+        message << head
+        while !done?(message) do
+            source = pick_source(head)
+            choice = source[head].sample
+            if choice.nil?
+                choice = pick_source(head).keys.sample
+            end
+            message << choice
+            head = choice
+        end
+
+        message.join(" ") + END_MARKS.sample
+    end
+
+    def done?(message)
+        raise NotImplementedError
+    end
+
+private
+    def is_cap(word)
+        CAPS.include?(word[0])
+    end
+
+    def is_end(word)
+        END_MARKS.include?(word[-1])
+    end
+
+    def pick_source(word)
+        if is_end(word) || is_cap(word)
+            @cap
+        else
+            @lower
+        end
+    end    
+
+    def remove_punctuation(str)
+        '''
+        Remove tail punctuation for easier chaining.
+        '''
+        chars = Regexp.escape("()[]-_,:\"*;")
+        str.gsub(/[#{chars}]/, "")
+    end
+
+    def collapse_heads(str)
+        '''
+        Make sentence less sparse by removing ending punctuation on head.
+        '''
+        #remove characters from the string
+        end_chars = Regexp.escape(".?!")
+        str.gsub(/[#{end_chars}]/, "")
+    end
+
+end
+
+class TwitterMarkov < Markov
+    def done?(message)
+        return message.join(" ").size > rand(130)
+
+    end
+end
+
+
--- pythontrek_markov.rb
+++ pythontrek_markov.rb
@@ -2,9 +2,12 @@
 make markov chain and then generate random text to tweet
 '''
 require './markov_model.rb'
+require './markov_model2.rb'
 require './tweet.rb'
+require 'rubygems'
+require 'twitter'
 
-def main()
+def main_functional()
 
     #read open and read file as a string 
     astr = open("raw_combo.txt").read()
@@ -20,6 +23,38 @@ def main()
 
 end
 
+
+def tweet_post(random_txt)
+    '''
+    function to post random text on twitter
+    '''
+    #pull Twitter keys
+    Twitter.configure do |config|
+        config.consumer_key = ENV['KEY']
+        config.consumer_secret = ENV['SECRET']
+        config.oauth_token = ENV['TOKEN_KEY'] 
+        config.oauth_token_secret =  ENV['TOKEN_SECRET']
+    end
+
+    #cuts off end of string if too long
+    if random_txt.length > 140
+        random_txt = random_txt[0...139] + endmark()
+    end
+
+    #submits tweet
+    Twitter.update(random_txt)
+
+end
+
+
+def main_object()
+    sample_text = open("raw_combo.txt").read()
+    generator = TwitterMarkov.new sample_text
+    random_txt = generator.generate()
+    puts random_txt
+    tweet_post(random_txt)
+end
+
 if __FILE__ == $0
-    main()
+    main_object()
 end
